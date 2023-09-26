b204dc7ca1ae95d5bf90cc25bad3c99807fa57f3
BrennerLittle <brennerlittle@cox.net>
2023-09-26 13:00:54 -0500

    Added Credits System

---
+++ Scripts/Credits.gd
@@ -0,0 +1,19 @@
+extends Node
+
+func _loadCredits():
+	var file = FileAccess.open("res://MiscFiles/credits.txt", FileAccess.READ)
+	var content = file.get_as_text()
+	return content
+# Called when the node enters the scene tree for the first time.
+func _ready():
+
+	$".".set_text(_loadCredits())
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$".".position.y-=1
+	if($".".position.y==-1000):
+		get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+	pass
--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -14,11 +14,16 @@ func _process(delta):
 func _on_item_clicked(index, at_position, mouse_button_index):
 	if(index==0):
 		#Sstarts game
+		get_tree().change_scene_to_file("res://Scenes/MapOne.tscn")
 		pass
 	if(index==1):
 		#settings
 		pass
 	if(index==2):
+		#credits
+		get_tree().change_scene_to_file("res://Scenes/Credits.tscn")
+		pass
+	if(index==4):
 		#quits game
 		get_tree().quit()
 	pass # Replace with function body.
